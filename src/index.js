0b7a0b820e389f9f396d6be0007b569c0ab9008b
ASyrotenko <andreysyrotenko@gmail.com>
2022-11-21 21:25:06 +0200

    show/hide loadMoreBtn on submit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import SearchApiService from './search-service.js';
+import Notiflix from 'notiflix';
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
   loadMoreBtn: document.querySelector('.load-more'),
+  galleryContainer: document.querySelector('.gallery'),
 };
 
 const searchApiService = new SearchApiService();
@@ -13,11 +15,65 @@ refs.loadMoreBtn.addEventListener('click', onLoadMore);
 function onSearch(e) {
   e.preventDefault();
 
+  hideLoadMoreBtn();
   searchApiService.query = e.currentTarget.elements.searchQuery.value;
-
-  searchApiService.fetchArticles();
+  searchApiService.resetPage();
+  searchApiService.fetchArticles().then(hits => {
+    clearMarkup();
+    if (hits.length === 0) {
+      return Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.',
+        { clickToClose: true }
+      );
+    }
+    renderSearchMarkup(hits);
+    showLoadMoreBtn();
+  });
 }
 
 function onLoadMore() {
-  searchApiService.fetchArticles();
+  searchApiService.fetchArticles().then(renderSearchMarkup);
+}
+
+function renderSearchMarkup(hits) {
+  const markup = hits
+    .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+      return `
+    <div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      <b>${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      <b>${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      <b>${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      <b>${downloads}</b>
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+
+  refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearMarkup() {
+  refs.galleryContainer.innerHTML = '';
+}
+
+function showLoadMoreBtn() {
+  refs.loadMoreBtn.classList.remove('is-hidden');
+}
+
+function hideLoadMoreBtn() {
+  refs.loadMoreBtn.classList.add('is-hidden');
 }
--- src/search-service.js
+++ src/search-service.js
@@ -5,20 +5,28 @@ export default class SearchApiService {
   }
 
   fetchArticles() {
-    console.log(this);
     const options = {
       API_KEY: '31493701-066eddf0638dc5b7781a5a354',
       URL: 'https://pixabay.com/api/',
     };
 
-    fetch(
-      `${options.URL}?key=${options.API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}`
+    return fetch(
+      `${options.URL}?key=${options.API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=200`
     )
       .then(response => response.json())
-      .then(data => {
-        this.page += 1;
-        console.log(this);
-      });
+      .then(({ hits }) => {
+        this.incrementPage();
+        return hits;
+      })
+      .catch(error => console.log(error.message));
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
   }
 
   get query() {
